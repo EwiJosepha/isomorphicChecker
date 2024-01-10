5897513d0e456bd584b5e6f59e3b69601bc333c6
ewi josepha <ewijosepha@gmail.com>
2024-01-10 16:18:51 +0100

    formatting and cleanning document

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,29 +1,30 @@
 // Javascript function to check if two
 // strings are isomorphic
 // catching the DOM
+
 const button = document.getElementById('submit-button')
 const displayResult = document.getElementById('resultt')
+
 // listening to a click button
 // Event handeler
-function isomorphicString (e) {
+
+function isomorphicString(e) {
   e.preventDefault()
   const s = document.getElementById('string-one').value
   const t = document.getElementById('string-two').value
   if (s.length !== t.length) {
-    displayResult.innerHTML = 'The two strings must bee the same'
+    displayResult.innerHTML = 'The two strings must be of equall lengths '
   }
   const mapa = new Map()
   const mapb = new Map()
   for (let i = 0; i < s.length; i++) {
     if (mapa.has(s[i])) {
       if (mapa.get(s[i]) !== t[i]) {
-        console.log(mapa.get(s[i]))
         displayResult.innerHTML = 'false'
         return
       }
     } else {
       mapa.set(s[i], t[i])
-      console.log(mapa)
     }
     if (mapb.has(t[i])) {
       if (mapb.get(t[i]) !== s[i]) {
@@ -38,3 +39,7 @@ function isomorphicString (e) {
 }
 
 button.addEventListener('click', isomorphicString)
+
+// map.has checks if a particular word or sentence has a particula word 
+//map.get gets the value froom a string and returns such values.
+//map.set sets given values to a string or senntence or better still it sets items to string
